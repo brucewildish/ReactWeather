8359f7d76f0196f3a9332f1801ff70186bae188f
bruce.wildish <bruce.wildish@scm.ca>
2018-01-05 14:18:22 -0500

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -3,10 +3,20 @@ var express = require('express');  	// provides access to express api via object
 
   // Create our app as an instance of the express class
 	var app = express();
+	const PORT = process.env.PORT || 3000;
+
+  // this part is for Heroku
+	app.use(function (req, res, next) {
+		if (req.headers['x-forwarded-proto'] === 'http') {
+			next();
+		} else {
+			res.redirect('http//' + req.hostname + req.url);
+		}
+	});
 
   app.use(express.static('public'));  	// use folder "public"
 
   // start server takes port and function to call once server is up
-  app.listen (3000, function() {
-		console.log('Express server is up on port 3000')
+  app.listen (PORT, function() {
+		console.log('Express server is up on port ' + PORT);
 });
